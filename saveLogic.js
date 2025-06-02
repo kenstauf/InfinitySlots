e256310697a2cd206135f01286b9e05e5219a254
kenstauf <kenstauf97@gmail.com>
2025-06-02 16:58:02 -0600

    Update saveLogic.js

--- saveLogic.js
+++ saveLogic.js
@@ -1,1 +1,22 @@
 //save logic here
+function saveGame() {
+  const saveData = {
+    coins: coins,
+    rowCount: rowCount,
+    totalWinnings: totalWinnings
+  };
+  localStorage.setItem("slotSaveData", JSON.stringify(saveData));
+}
+//----------------------
+//load logic here
+function loadGame() {
+  const save = JSON.parse(localStorage.getItem("slotSaveData"));
+  if (save) {
+    coins = save.coins;
+    rowCount = save.rowCount;
+    totalWinnings = save.totalWinnings;
+
+    updateStatsPanel();
+    initReels();
+  }
+}
